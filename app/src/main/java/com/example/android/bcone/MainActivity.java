680786ef27c00998d3b978ddd7906fce743e422f
jsbaidwan <jsbaidwan5@gmail.com>
2017-02-14 10:34:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/bcone/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.bcone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent childOne = new Intent(MainActivity.this, activity_child_1.class);
+                startActivity(childOne);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/bcone/activity_child_1.java
@@ -0,0 +1,13 @@
+package com.example.android.bcone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_child_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.child_activity_1);
+    }
+}
