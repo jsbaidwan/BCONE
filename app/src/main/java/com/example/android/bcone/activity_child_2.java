e28f0838ed6b56d74cbf2df35520ee6cbfa975e7
jsbaidwan <jsbaidwan5@gmail.com>
2017-02-16 14:09:59 +0530

    added more static popUp

--- app/src/main/java/com/example/android/bcone/activity_child_2.java
+++ app/src/main/java/com/example/android/bcone/activity_child_2.java
@@ -7,17 +7,49 @@ import android.view.View;
 import android.widget.Button;
 
 public class activity_child_2 extends AppCompatActivity {
-    Button popUp;
-
+    Button popUp1;
+    Button popUp2;
+    Button popUp3;
+    Button popUp4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.child_layout_2);
-        popUp = (Button) findViewById(R.id.audit_now);
+        popUp1 = (Button) findViewById(R.id.audit_now_1);
+        popUp2 = (Button) findViewById(R.id.audit_now_2);
+        popUp3 = (Button) findViewById(R.id.audit_now_3);
+        popUp4 = (Button) findViewById(R.id.audit_now_4);
 
-        popUp.setOnClickListener(new View.OnClickListener() {
+        popUp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent popUp = new Intent(activity_child_2.this, Popup.class);
+                startActivity(popUp);
+
+            }
+        });
+
+        popUp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent popUp = new Intent(activity_child_2.this, Popup.class);
+                startActivity(popUp);
+
+            }
+        });
+
+        popUp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent popUp = new Intent(activity_child_2.this, Popup.class);
+                startActivity(popUp);
+
+            }
+        });
+
+        popUp4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent popUp = new Intent(activity_child_2.this, Popup.class);
